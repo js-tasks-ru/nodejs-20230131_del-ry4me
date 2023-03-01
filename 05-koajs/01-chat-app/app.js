54789afa353f35c3e7189aa436d09b1786ee27d8
Artem Kuteynikov <del-ry4me@yandex.ru>
2023-03-01 15:36:06 +0300

    task 05-01 with tutors help

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -6,33 +6,54 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
+const { result } = require('lodash');
 const router = new Router();
 
-let subscribers = Object.create(null);
+/**let subscribers = Object.create(null);
 
 
 router.get('/subscribe', async (ctx, next) => {
-    let id = ctx.query.r;
-    subscribers[id] = ctx;
-
-    const response = await promise;
-
-    for (id in subscribers) {
-        if (response == '') {
-            return;
-        }
-        ctx.body = response;
-    }
-    subscribers = Object.create(null);
-
+    await promise;
+    let resp = promise;
+    ctx.body = resp;
 
 });
 
 let promise = new Promise((resolve) => {router.post('/publish', async (ctx, next) => {
-        const message = ctx.request.body.message;
-        resolve(message);
-});
-});
+    let message = ctx.request.body.message;
+    resolve(message);
+    });
+})
+*/
+const clients = new Set();
+router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise((resolve, reject) => {
+      clients.add(resolve);
+  
+      ctx.res.on('close', function() {
+        clients.delete(resolve);
+        resolve();
+      });
+    });
+  
+    ctx.body = message;
+  });
+
+  router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+  
+    if (!message) {
+      ctx.throw(400, 'required field `message` is missing');
+    }
+  
+    clients.forEach(resolve => {
+      resolve(message);
+    });
+  
+    clients.clear();
+  
+    ctx.body = 'ok';
+  });
 
 app.use(router.routes());
 
